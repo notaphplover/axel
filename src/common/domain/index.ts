60ad1a6bf9c7287662a5721a51655daf21a6dcf9
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2020-08-27 00:11:22 +0200

    feat(common): add interactor interface

--- src/common/domain/index.ts
+++ src/common/domain/index.ts
@@ -1,9 +1,10 @@
+import { Interactor } from './interactor/Interactor';
 import { SearchRepository } from './db/SearchRepository';
 import { Server } from './server/Server';
 import { floatToInt } from './parse/floatToInt';
 import { hasValue } from './utils/hasValue';
 
-export { SearchRepository, Server };
+export { Interactor, SearchRepository, Server };
 
 // eslint-disable-next-line @typescript-eslint/typedef
 export const common = {
