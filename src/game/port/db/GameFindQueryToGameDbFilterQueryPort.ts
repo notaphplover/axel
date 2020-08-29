15bf7199245613b817d476115fe853dccd49acbc
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2020-08-29 10:33:39 +0200

    refactor(game): update GameFindQueryToGameDbFilterQueryPort to be injectable

--- src/game/port/db/GameFindQueryToGameDbFilterQueryPort.ts
+++ src/game/port/db/GameFindQueryToGameDbFilterQueryPort.ts
@@ -3,9 +3,11 @@ import { GameDb } from '../../adapter/db/model/GameDb';
 import { GameFindQuery } from '../../domain/query/GameFindQuery';
 import { Port } from '../../../common/port';
 import { common } from '../../../common/domain';
+import { injectable } from 'inversify';
 
 const hasValue: (object: unknown) => boolean = common.utils.hasValue;
 
+@injectable()
 export class GameFindQueryToGameDbFilterQueryPort
   implements Port<GameFindQuery, FilterQuery<GameDb>> {
   public transform(input: GameFindQuery): FilterQuery<GameDb> {
