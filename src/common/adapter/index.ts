efc6100558bef51170537372ce15520fe18a9e1a
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2020-09-02 22:38:04 +0200

    feat(common): add MongooseInsertRepository to exported types

--- src/common/adapter/index.ts
+++ src/common/adapter/index.ts
@@ -1,11 +1,13 @@
 import { ApiVersion } from './api/ApiVersion';
 import { FastifyRequestHandler } from './server/model/FastifyRequestHandler';
 import { FastifyRouter } from './server/model/FastifyRouter';
+import { MongooseInsertRepository } from './db/MongooseInsertRepository';
 import { MongooseSearchRepository } from './db/MongooseSearchRepository';
 
 export {
   ApiVersion,
   FastifyRequestHandler,
   FastifyRouter,
+  MongooseInsertRepository,
   MongooseSearchRepository,
 };
