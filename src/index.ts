4feb1d2da0ef76ef3e73aeada0b35765a47163dd
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2020-09-02 11:45:48 +0200

    refactor(common): update entrypoint to contain an async arrow

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ import { COMMON_DOMAIN_TYPES } from './common/domain/config/types';
 import { Server } from './common/domain';
 import { container } from './common/adapter/config/container';
 
-const httpServer: Server = container.get(COMMON_DOMAIN_TYPES.SERVER);
+void (async () => {
+  const httpServer: Server = container.get(COMMON_DOMAIN_TYPES.SERVER);
 
-void httpServer.bootstrap();
+  await httpServer.bootstrap();
+})();
