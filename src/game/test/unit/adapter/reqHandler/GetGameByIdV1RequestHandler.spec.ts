3d100a244607f65e3cf9186b8a48f2f173b792ac
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2020-09-08 22:14:25 +0200

    test(game): update GetGameByIdV1RequestHandler with tests when a game is not found

--- src/game/test/unit/adapter/reqHandler/GetGameByIdV1RequestHandler.spec.ts
+++ src/game/test/unit/adapter/reqHandler/GetGameByIdV1RequestHandler.spec.ts
@@ -6,6 +6,7 @@ import { Game } from '../../../../domain/model/Game';
 import { GameApiV1 } from '../../../../adapter/api/model/GameApiV1';
 import { GameFindQuery } from '../../../../domain/query/GameFindQuery';
 import { GetGameByIdV1RequestHandler } from '../../../../adapter/server/reqHandler/GetGameByIdV1RequestHandler';
+import { StatusCodes } from 'http-status-codes';
 import { commonTest } from '../../../../../common/test';
 import { gameApiV1FixtureFactory } from '../../../fixtures/adapter/api/model/gameApiV1.fixture';
 import { gameFixtureFactory } from '../../../fixtures/domain/model/game.fixture';
@@ -68,6 +69,33 @@ describe(GetGameByIdV1RequestHandler.name, () => {
         expect(replyFixture.send).toHaveBeenCalledTimes(1);
         expect(replyFixture.send).toHaveBeenCalledWith(
           gameApiV1FixtureFactory.get(),
+        );
+      });
+    });
+
+    describe('.when called and no game is found', () => {
+      let requestFixture: FastifyRequest;
+      let replyFixture: FastifyReply;
+
+      beforeAll(async () => {
+        requestFixture = ({
+          params: { gameId: 'test-game-id' },
+        } as Partial<FastifyRequest>) as FastifyRequest;
+        replyFixture = commonTest.fixtures.adapter.server.fastifyReplyFixtureFactory.get();
+
+        (findGameInteractor.interact as jest.Mock).mockResolvedValueOnce(null);
+
+        await getGameByIdV1RequestHandler.handle(requestFixture, replyFixture);
+      });
+
+      it('must call reply.code with not found code', () => {
+        expect(replyFixture.code).toHaveBeenCalledTimes(1);
+        expect(replyFixture.code).toHaveBeenCalledWith(StatusCodes.NOT_FOUND);
+      });
+      it('must call reply.send with a text mesage', () => {
+        expect(replyFixture.send).toHaveBeenCalledTimes(1);
+        expect(replyFixture.send).toHaveBeenCalledWith(
+          expect.stringContaining('not found'),
         );
       });
     });
