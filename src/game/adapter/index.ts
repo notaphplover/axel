5546d6ccb425157426a1f523a3c5356afeb6aaf2
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2020-09-09 00:44:10 +0200

    refactor(global): update adapter objects to include its container

--- src/game/adapter/index.ts
+++ src/game/adapter/index.ts
@@ -1,8 +1,10 @@
 import { GAME_ADAPTER_PUBLIC_TYPES } from './config/types';
+import { gameContainer } from './config/container';
 
 // eslint-disable-next-line @typescript-eslint/typedef
 export const gameAdapter = {
   config: {
+    container: gameContainer,
     types: GAME_ADAPTER_PUBLIC_TYPES,
   },
 };
--- src/json-schema/adapter/index.ts
+++ src/json-schema/adapter/index.ts
@@ -1,9 +1,13 @@
 import { JSON_SCHEMA_ADAPTER_PUBLIC_TYPES } from './config/types';
 import { JsonSchemaValidator } from './validator/JsonSchemaValidator';
+import { jsonSchemaContainer } from './config/container';
 
 export { JsonSchemaValidator };
 
 // eslint-disable-next-line @typescript-eslint/typedef
 export const jsonSchemaAdapter = {
-  config: { types: JSON_SCHEMA_ADAPTER_PUBLIC_TYPES },
+  config: {
+    container: jsonSchemaContainer,
+    types: JSON_SCHEMA_ADAPTER_PUBLIC_TYPES,
+  },
 };
