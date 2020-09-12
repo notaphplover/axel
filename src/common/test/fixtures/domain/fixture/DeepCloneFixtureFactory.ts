23b6670b31be2be9befafd1ca5a75ff428302109
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2020-09-12 11:46:06 +0200

    test(fixtures): add DeepCloneFixtureFactory

---
+++ src/common/test/fixtures/domain/fixture/DeepCloneFixtureFactory.ts
@@ -0,0 +1,10 @@
+import * as _ from 'lodash';
+import { PrototypeBasedFixtureFactory } from './PrototypeBasedFixtureFactory';
+
+export class DeepCloneFixtureFactory<
+  TData
+> extends PrototypeBasedFixtureFactory<TData> {
+  public get(): TData {
+    return _.cloneDeep(this.data);
+  }
+}
--- src/common/test/index.ts
+++ src/common/test/index.ts
@@ -1,8 +1,13 @@
+import { DeepCloneFixtureFactory } from './fixtures/domain/fixture/DeepCloneFixtureFactory';
 import { FixtureFactory } from './fixtures/domain/fixture/FixtureFactory';
 import { PrototypeBasedFixtureFactory } from './fixtures/domain/fixture/PrototypeBasedFixtureFactory';
 import { fastifyReplyFixtureFactory } from '../../layer-modules/server/test/fixtures/fastify.fixture';
 
-export { FixtureFactory, PrototypeBasedFixtureFactory };
+export {
+  DeepCloneFixtureFactory,
+  FixtureFactory,
+  PrototypeBasedFixtureFactory,
+};
 
 // eslint-disable-next-line @typescript-eslint/typedef
 export const commonTest = {
