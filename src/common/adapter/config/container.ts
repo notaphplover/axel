d25126698183a6bd796298cf59804be68f174c6a
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2020-09-12 11:06:24 +0200

    feat(common): user container is load into the main container

--- src/common/adapter/config/container.ts
+++ src/common/adapter/config/container.ts
@@ -4,6 +4,7 @@ import { gameAdapter } from '../../../game/adapter';
 import { jsonSchemaAdapter } from '../../../json-schema/adapter';
 import { jwtAdapter } from '../../../jwt/adapter';
 import { serverAdapter } from '../../../layer-modules/server/adapter';
+import { userAdapter } from '../../../user/adapter';
 
 export const container: Container = new Container();
 
@@ -12,3 +13,4 @@ container.load(gameAdapter.config.container);
 container.load(jsonSchemaAdapter.config.container);
 container.load(jwtAdapter.config.container);
 container.load(serverAdapter.config.container);
+container.load(userAdapter.config.container);
