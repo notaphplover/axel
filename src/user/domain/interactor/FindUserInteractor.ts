76e2bed9111953e71415c8b1eb9828de340a42e6
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2020-09-12 11:33:17 +0200

    refactor(user): add Implementation declaration to FindUserInteractor

--- src/user/domain/interactor/FindUserInteractor.ts
+++ src/user/domain/interactor/FindUserInteractor.ts
@@ -1,11 +1,13 @@
 import { inject, injectable } from 'inversify';
+import { Interactor } from '../../../common/domain';
 import { SearchRepository } from '../../../layer-modules/db/domain';
 import { USER_DOMAIN_TYPES } from '../config/types';
 import { User } from '../model/User';
 import { UserFindQuery } from '../query/UserFindQuery';
 
 @injectable()
-export class FindUserInteractor {
+export class FindUserInteractor
+  implements Interactor<UserFindQuery, Promise<User | null>> {
   constructor(
     @inject(USER_DOMAIN_TYPES.repository.USER_SEARCH_REPOSITORY)
     private readonly userSearchRepository: SearchRepository<
