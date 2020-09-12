645b1f7441ca537a4fb3e68bf6f293c6d6eed907
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2020-09-12 18:55:55 +0200

    feat(user): publish userAdapter publish types

--- src/user/adapter/index.ts
+++ src/user/adapter/index.ts
@@ -1,8 +1,10 @@
+import { USER_ADAPTER_PUBLIC_TYPES } from './config/types';
 import { userContainer } from './config/container';
 
 // eslint-disable-next-line @typescript-eslint/typedef
 export const userAdapter = {
   config: {
     container: userContainer,
+    types: USER_ADAPTER_PUBLIC_TYPES,
   },
 };
