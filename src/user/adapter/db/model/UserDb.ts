f9305975fc05a560da6552bff6b219822955f461
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2020-09-14 18:11:37 +0200

    fix(user): update UserDb to have the right scheme

--- src/user/adapter/db/model/UserDb.ts
+++ src/user/adapter/db/model/UserDb.ts
@@ -5,13 +5,16 @@ export interface UserDb extends Document {
   _id: Types.ObjectId;
 
   email: string;
+  hash: string;
   roles: UserRole[];
   username: string;
-  hash: string;
 }
 
 const userDbSchema: Schema = new Schema({
-  round: { type: Number, required: true },
+  email: { type: String, required: true },
+  hash: { type: String, required: true },
+  roles: { type: Array, of: String, required: true },
+  username: { type: String, required: true },
 });
 
 export const userDbModel: Model<UserDb> = mongoose.model<UserDb>(
