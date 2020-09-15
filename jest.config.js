53ff029f6ac727f919ccd21ae6415c1d17c6aaf2
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2020-09-15 15:21:56 +0200

    chore(test): update jest config with integration test config

--- jest.config.js
+++ jest.config.js
@@ -30,6 +30,33 @@ module.exports = {
       },
     },
     {
+      displayName: 'Integration',
+      collectCoverageFrom: ['**/*.ts'],
+      coveragePathIgnorePatterns: [
+        '/node_modules/',
+        '<rootDir>/*/test/',
+        '<rootDir>/layer-modules/*/test/',
+      ],
+      coverageThreshold: {
+        global: {
+          branches: 70,
+          functions: 70,
+          lines: 70,
+          statements: 70,
+        },
+      },
+      moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+      rootDir: 'src',
+      testEnvironment: 'node',
+      testMatch: [
+        '<rootDir>/*/test/integration/**/*.spec.ts',
+        '<rootDir>/layer-modules/*/test/integration/**/*.spec.ts',
+      ],
+      transform: {
+        '^.+\\.ts?$': 'ts-jest',
+      },
+    },
+    {
       displayName: 'End to End',
       collectCoverageFrom: ['**/*.ts'],
       coveragePathIgnorePatterns: [
