4e56a0b4b306cafce089b7353d981f5472563978
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2020-09-16 10:57:03 +0200

    feat(common): add jest custom describe function

--- src/common/test/index.ts
+++ src/common/test/index.ts
@@ -1,6 +1,7 @@
 import { DeepCloneFixtureFactory } from './fixtures/domain/fixture/DeepCloneFixtureFactory';
 import { FixtureFactory } from './fixtures/domain/fixture/FixtureFactory';
 import { PrototypeBasedFixtureFactory } from './fixtures/domain/fixture/PrototypeBasedFixtureFactory';
+import { customDescribe } from './integration/utills/customDescribe';
 import { fastifyReplyFixtureFactory } from '../../layer-modules/server/test/fixtures/fastify.fixture';
 
 export {
@@ -12,4 +13,5 @@ export {
 // eslint-disable-next-line @typescript-eslint/typedef
 export const commonTest = {
   fixtures: { adapter: { server: { fastifyReplyFixtureFactory } } },
+  integration: { utils: { customDescribe } },
 };
---
+++ src/common/test/integration/utills/customDescribe.ts
@@ -0,0 +1,75 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+function innerDescribeWrapper(
+  describeFn: jest.Describe,
+  name: string,
+  describeCallback: () => void,
+  setupCallback: () => void,
+): void {
+  describeFn(name, () => {
+    setupCallback();
+
+    describeCallback();
+  });
+}
+
+function innerEachWrapper(
+  describeFn: jest.Describe,
+  setupCallback: () => void,
+): jest.Each {
+  return (
+    casesOrStrings: ReadonlyArray<any> | TemplateStringsArray,
+    ...placeholders: any[]
+  ) => (name: string, fn: (...args: any[]) => any, timeout?: number): void => {
+    innerDescribeWrapper(
+      describeFn,
+      name,
+      () => {
+        if (placeholders.length === 0) {
+          return describeFn.each(casesOrStrings)(name, fn, timeout);
+        } else {
+          return describeFn.each(
+            casesOrStrings as TemplateStringsArray,
+            ...placeholders,
+          )(name, fn, timeout);
+        }
+      },
+      setupCallback,
+    );
+  };
+}
+
+export const customDescribe: (
+  describeFn: jest.Describe,
+  setupCallback: () => void,
+) => jest.Describe = (
+  describeFn: jest.Describe,
+  setupCallback: () => void,
+): jest.Describe =>
+  new Proxy(describeFn, {
+    apply: (
+      target: jest.Describe,
+      thisArg: jest.Describe,
+      argArray: [string, () => void],
+    ): void => {
+      const [name, describeCallback]: [string, () => void] = argArray;
+      innerDescribeWrapper(target, name, describeCallback, setupCallback);
+    },
+    get: (
+      target: jest.Describe,
+      handler: string,
+    ): jest.Describe | jest.Each => {
+      switch (handler) {
+        case 'each':
+          return innerEachWrapper(target, setupCallback);
+        case 'only':
+          return customDescribe(target.only, setupCallback);
+        case 'skip':
+          return customDescribe(target.skip, setupCallback);
+        default:
+          return ((target as unknown) as Record<
+            string,
+            jest.Describe | jest.Each
+          >)[handler];
+      }
+    },
+  });
