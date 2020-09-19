c61863eaf793caed28e220594f0419718565d09a
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2020-09-19 08:12:52 +0200

    style(db): update mongooseIntegrationDescribe with missing types

--- src/layer-modules/db/test/integration/utils/mongooseIntegrationDescribe.ts
+++ src/layer-modules/db/test/integration/utils/mongooseIntegrationDescribe.ts
@@ -4,18 +4,22 @@ import { container } from '../../../../../common/adapter/config/container';
 
 export const mongooseIntegrationDescribe: jest.Describe = commonTest.integration.utils.customDescribe(
   describe,
-  () => {
+  (): void => {
     let mongooseConnector: MongooseConector;
 
-    beforeAll(async () => {
-      mongooseConnector = container.get(
-        dbAdapter.config.types.db.MONGOOSE_CONNECTOR,
-      );
-      await mongooseConnector.connect();
-    });
+    beforeAll(
+      async (): Promise<void> => {
+        mongooseConnector = container.get(
+          dbAdapter.config.types.db.MONGOOSE_CONNECTOR,
+        );
+        await mongooseConnector.connect();
+      },
+    );
 
-    afterAll(async () => {
-      await mongooseConnector.close();
-    });
+    afterAll(
+      async (): Promise<void> => {
+        await mongooseConnector.close();
+      },
+    );
   },
 );
