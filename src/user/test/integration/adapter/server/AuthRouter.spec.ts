e57ee9cfaefbce11c7bb91257f5c8bf5e91dd4c3
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2020-09-20 19:59:38 +0200

    test(user): add AuthRouter tests

---
+++ src/user/test/integration/adapter/server/AuthRouter.spec.ts
@@ -0,0 +1,73 @@
+/* eslint-disable @typescript-eslint/unbound-method */
+import 'reflect-metadata';
+import { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify';
+import {
+  FastifyRequestHandler,
+  FastifyRouter,
+} from '../../../../../layer-modules/server/adapter';
+import {
+  FastifyServerTestOutputParam,
+  serverTest,
+} from '../../../../../layer-modules/server/test';
+import { AuthRouter } from '../../../../adapter/server/router/AuthRouter';
+import { FastifyServerTest } from '../../../../../layer-modules/server/test';
+import { Response as LightMyRequestResponse } from 'light-my-request';
+
+const fastifyIntegrationDescribeGenerator: (
+  router: FastifyRouter,
+  output: FastifyServerTestOutputParam,
+) => jest.Describe =
+  serverTest.integration.utils.fastifyIntegrationDescribeGenerator;
+
+const fastifyServerTestOutputParam: FastifyServerTestOutputParam = {};
+
+const postAuthV1RequestHandlerMock: FastifyRequestHandler = {
+  handle: jest.fn(),
+};
+
+const authRouter: FastifyRouter = new AuthRouter(postAuthV1RequestHandlerMock);
+
+fastifyIntegrationDescribeGenerator(authRouter, fastifyServerTestOutputParam)(
+  AuthRouter.name,
+  () => {
+    let fastifyInstance: FastifyInstance;
+
+    describe('POST auth', () => {
+      beforeAll(async () => {
+        fastifyInstance = (fastifyServerTestOutputParam.value as FastifyServerTest)
+          .fastify as FastifyInstance;
+      });
+
+      describe('when called', () => {
+        let responseBodyFixture: Record<string, unknown>;
+        let response: unknown;
+
+        beforeAll(async () => {
+          responseBodyFixture = {
+            foo: 'bar',
+          };
+          (postAuthV1RequestHandlerMock.handle as jest.Mock).mockImplementationOnce(
+            async (
+              request: FastifyRequest,
+              reply: FastifyReply,
+            ): Promise<void> => {
+              await reply.send(responseBodyFixture);
+            },
+          );
+
+          response = await fastifyInstance.inject({
+            method: 'POST',
+            url: '/v1/auth/tokens',
+          });
+        });
+
+        it('must call postAuthV1RequestHandler.handle to handle the request', () => {
+          expect(postAuthV1RequestHandlerMock.handle).toHaveBeenCalledTimes(1);
+          expect(
+            JSON.parse((response as LightMyRequestResponse).body),
+          ).toStrictEqual(responseBodyFixture);
+        });
+      });
+    });
+  },
+);
