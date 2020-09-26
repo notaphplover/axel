4c39a12114f3d77ee4093d9fcaecdee3c91374c3
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2020-09-26 15:27:47 +0200

    feat(server): update server module to export FastifyAuthenticator

--- src/layer-modules/server/adapter/index.ts
+++ src/layer-modules/server/adapter/index.ts
@@ -1,10 +1,16 @@
+import { FastifyAuthenticator } from './auth/FastifyAuthenticator';
 import { FastifyPortListeningServer } from './FastifyPortListeningServer';
 import { FastifyRequestHandler } from './FastifyRequestHandler';
 import { FastifyRouter } from './FastifyRouter';
 import { SERVER_ADAPTER_PUBLIC_TYPES } from './config/types';
 import { serverContainer } from './config/container';
 
-export { FastifyRequestHandler, FastifyRouter, FastifyPortListeningServer };
+export {
+  FastifyAuthenticator,
+  FastifyRequestHandler,
+  FastifyRouter,
+  FastifyPortListeningServer,
+};
 
 // eslint-disable-next-line @typescript-eslint/typedef
 export const serverAdapter = {
