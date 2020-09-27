7f28f22fba0ee51da1054e0b281f8601ed9a39c1
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2020-09-27 12:38:10 +0200

    feat(game): update CardRouter to require an user admin authentication at POST cards

--- src/game/adapter/server/router/card/CardRouter.ts
+++ src/game/adapter/server/router/card/CardRouter.ts
@@ -1,17 +1,27 @@
-import { FastifyInstance, FastifyServerOptions } from 'fastify';
+import {
+  FastifyInstance,
+  FastifyReply,
+  FastifyRequest,
+  FastifyServerOptions,
+} from 'fastify';
 import {
   FastifyRequestHandler,
   FastifyRouter,
 } from '../../../../../layer-modules/server/adapter';
 import { inject, injectable } from 'inversify';
 import { ApiVersion } from '../../../../../layer-modules/api/adapter';
+import { FastifyUserAuthenticator } from '../../../../../user/adapter/auth/FastifyUserAuthenticator';
 import { GAME_ADAPTER_TYPES } from '../../../config/types';
+import { UserRole } from '../../../../../user/domain/model/UserRole';
+import { userAdapter } from '../../../../../user/adapter';
 
 const CARD_ROUTER_PATH_PREFIX: string = 'cards';
 
 @injectable()
 export class CardRouter implements FastifyRouter {
   constructor(
+    @inject(userAdapter.config.types.auth.FASTIFY_USER_AUTHENTICATOR)
+    private readonly fastifyUserAuthenticator: FastifyUserAuthenticator,
     @inject(
       GAME_ADAPTER_TYPES.server.reqHandler.card.GET_CARDS_V1_REQUEST_HANDLER,
     )
@@ -42,6 +52,11 @@ export class CardRouter implements FastifyRouter {
       ),
     });
     server.post(`/${CARD_ROUTER_PATH_PREFIX}`, {
+      onRequest: async (request: FastifyRequest, reply: FastifyReply) => {
+        await this.fastifyUserAuthenticator.authenticate(request, reply, [
+          UserRole.ADMIN,
+        ]);
+      },
       handler: this.postCardV1RequestHandler.handle.bind(
         this.postCardV1RequestHandler,
       ),
