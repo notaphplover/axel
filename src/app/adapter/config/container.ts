2a003449707eece0bcf5c4d5cff1d038b6bb7a18
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2020-10-12 15:13:07 +0200

    feat(app): add GetStatusV1RequestHandler to DI

--- src/app/adapter/config/container.ts
+++ src/app/adapter/config/container.ts
@@ -1,6 +1,13 @@
 import { ContainerModule, interfaces } from 'inversify';
 import { APP_ADAPTER_TYPES } from './types';
 import { AppEnvLoader } from '../env/AppEnvLoader';
+import { GetStatusV1RequestHandler } from '../server/reqHandler/GetStatusV1RequestHandler';
+
+function bindAdapter(bind: interfaces.Bind): void {
+  bind(APP_ADAPTER_TYPES.server.reqHandler.GET_STATUS_V1_REQUEST_HANDLER).to(
+    GetStatusV1RequestHandler,
+  );
+}
 
 function bindDomain(bind: interfaces.Bind): void {
   bind(APP_ADAPTER_TYPES.env.APP_ENV_LOADER)
@@ -10,6 +17,7 @@ function bindDomain(bind: interfaces.Bind): void {
 
 export const appContainer: ContainerModule = new ContainerModule(
   (bind: interfaces.Bind) => {
+    bindAdapter(bind);
     bindDomain(bind);
   },
 );
--- src/app/adapter/config/types.ts
+++ src/app/adapter/config/types.ts
@@ -3,6 +3,11 @@ export const APP_ADAPTER_TYPES = {
   env: {
     APP_ENV_LOADER: Symbol.for('AppEnvLoader'),
   },
+  server: {
+    reqHandler: {
+      GET_STATUS_V1_REQUEST_HANDLER: Symbol.for('GetStatusV1RequestHandler'),
+    },
+  },
 };
 
 // eslint-disable-next-line @typescript-eslint/typedef
