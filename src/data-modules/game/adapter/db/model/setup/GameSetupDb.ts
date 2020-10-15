f49a207c57a954450d055f59cb2b560ca1bffe0e
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2020-10-15 23:23:37 +0200

    feat(game): add GameSetupDb

---
+++ src/data-modules/game/adapter/db/model/setup/GameSetupDb.ts
@@ -0,0 +1,40 @@
+import mongoose, {
+  Document,
+  Model,
+  Schema,
+  SchemaDefinition,
+  Types,
+} from 'mongoose';
+import { GameFormat } from '../../../../domain/model/GameFormat';
+import { PlayerSetup } from '../../../../domain/model/setup/PlayerSetup';
+import { cardDeckDbSchemaDefinition } from '../deck/CardDeckDb';
+
+export interface GameSetupDb extends Document {
+  _id: Types.ObjectId;
+  format: GameFormat;
+  playerSetups: PlayerSetup[];
+  playerSlots: number;
+}
+
+const playerSetupDbSchemaDefinition: SchemaDefinition = {
+  deck: { type: cardDeckDbSchemaDefinition, required: true },
+  userId: { type: Schema.Types.ObjectId, required: true },
+};
+
+const gameSetupDbSchemaDefinition: SchemaDefinition = {
+  format: { type: String, required: true },
+  playerSetups: {
+    type: Array,
+    of: playerSetupDbSchemaDefinition,
+    required: true,
+  },
+  playerSlots: { type: Number },
+};
+
+const gameSetupDbSchema: Schema = new Schema(gameSetupDbSchemaDefinition);
+
+export const gameSetupDbModel: Model<GameSetupDb> = mongoose.model<GameSetupDb>(
+  'GameSetup',
+  gameSetupDbSchema,
+  'gameSetup',
+);
