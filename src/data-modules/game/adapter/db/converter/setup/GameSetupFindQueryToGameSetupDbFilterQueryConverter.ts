ca2df82a0c0d8d388377580693858ee182b57e53
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2020-10-16 14:26:53 +0200

    feat(game): update GameSetupFindQueryToGameSetupDbFilterQueryConverter with playerSetups and playerSlots fields

--- src/data-modules/game/adapter/db/converter/setup/GameSetupFindQueryToGameSetupDbFilterQueryConverter.ts
+++ src/data-modules/game/adapter/db/converter/setup/GameSetupFindQueryToGameSetupDbFilterQueryConverter.ts
@@ -2,6 +2,7 @@ import { FilterQuery, MongooseFilterQuery, Types } from 'mongoose';
 import { Converter } from '../../../../../../common/domain';
 import { GameSetupDb } from '../../model/setup/GameSetupDb';
 import { GameSetupFindQuery } from '../../../../domain/query/setup/GameSetupFindQuery';
+import { GameSetupFindQueryPlayerSetup } from '../../../../domain/query/setup/GameSetupFindQueryPlayerSetup';
 import { FilterQuery as MongoDbFilterQuery } from 'mongodb';
 import { hasValue } from '../../../../../../common/domain/utils/hasValue';
 import { injectable } from 'inversify';
@@ -25,6 +26,22 @@ export class GameSetupFindQueryToGameSetupDbFilterQueryConverter
 
     if (hasValue(input.ownerUserId)) {
       andFilterQuery.push({ ownerUserId: input.ownerUserId });
+    }
+
+    if (hasValue(input.playerSetups) && input.playerSetups.length > 0) {
+      const playerSetupsFilterQuery: MongoDbFilterQuery<
+        GameSetupDb
+      >[] = input.playerSetups.map(
+        (playerSetup: GameSetupFindQueryPlayerSetup) => {
+          return { 'playerSetup.userId': playerSetup.userId };
+        },
+      );
+
+      andFilterQuery.push({ $and: playerSetupsFilterQuery });
+    }
+
+    if (hasValue(input.playerSlots)) {
+      andFilterQuery.push({ playerSlots: input.playerSlots });
     }
 
     return filterQuery;
