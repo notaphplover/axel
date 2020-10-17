59ed65211d4160ea6726af6cbaa9535552c81a54
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2020-10-17 23:50:25 +0200

    feat(game): add BasicGameSetupDb

---
+++ src/data-modules/game/adapter/db/model/setup/BasicGameSetupDb.ts
@@ -0,0 +1,4 @@
+import { GameSetupDb } from './GameSetupDb';
+import { PlayerReference } from '../../../../domain/model/setup/PlayerReference';
+
+export type BasicGameSetupDb = GameSetupDb<PlayerReference>;
