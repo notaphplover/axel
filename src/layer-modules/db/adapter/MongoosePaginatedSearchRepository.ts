c122ec72b379ace57a5eece8b90ba24c51f7b962
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2020-10-19 14:24:46 +0200

    feat(db): add MongoosePaginatedSearchRepository

---
+++ src/layer-modules/db/adapter/MongoosePaginatedSearchRepository.ts
@@ -0,0 +1,40 @@
+import { Converter, Filter } from '../../../common/domain';
+import { Document, FilterQuery, Model } from 'mongoose';
+import { MongooseProjectionPaginatedSearchRepository } from './MongooseProjectionPaginatedSearchRepository';
+import { SearchRepository } from '../domain/SearchRepository';
+import { injectable } from 'inversify';
+
+@injectable()
+export abstract class MongoosePaginatedSearchRepository<
+    TModel,
+    TModelDb extends Document,
+    TQuery
+  >
+  extends MongooseProjectionPaginatedSearchRepository<
+    TModel,
+    TModelDb,
+    TModelDb,
+    TQuery
+  >
+  implements SearchRepository<TModel, TQuery> {
+  constructor(
+    protected readonly model: Model<TModelDb>,
+    protected readonly modelDbToModelConverter: Converter<
+      TModelDb,
+      TModel | Promise<TModel>
+    >,
+    protected readonly queryToFilterQueryConverter: Converter<
+      TQuery,
+      FilterQuery<TModelDb> | Promise<FilterQuery<TModelDb>>
+    >,
+    protected readonly postSearchFilter: Filter<TModelDb, TQuery> | null = null,
+  ) {
+    super(
+      model,
+      modelDbToModelConverter,
+      queryToFilterQueryConverter,
+      null,
+      postSearchFilter,
+    );
+  }
+}
