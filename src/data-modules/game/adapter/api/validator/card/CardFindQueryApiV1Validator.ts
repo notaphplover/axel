a5ba5abe1c2e1f2caf8730219bd42376784fa199
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2020-10-20 20:27:36 +0200

    feat(game): update CardFindQueryApiV1Validator to be a JoiObjectValidator

--- src/data-modules/game/adapter/api/validator/card/CardFindQueryApiV1Validator.ts
+++ src/data-modules/game/adapter/api/validator/card/CardFindQueryApiV1Validator.ts
@@ -1,27 +1,21 @@
 import { inject, injectable } from 'inversify';
-import Ajv from 'ajv';
 import { CardFindQueryApiV1 } from '../../query/card/CardFindQueryApiV1';
-import { JsonSchemaFileValidator } from '../../../../../../json-schema/adapter';
-import { join } from 'path';
-import { jsonSchemaAdapter } from '../../../../../../json-schema/adapter';
+import { GAME_ADAPTER_TYPES } from '../../../config/types';
+import Joi from 'joi';
+import { JoiObjectValidator } from '../../../../../../integration-modules/joi/adapter';
 
 @injectable()
-export class CardFindQueryApiV1Validator extends JsonSchemaFileValidator<
+export class CardFindQueryApiV1Validator extends JoiObjectValidator<
   CardFindQueryApiV1
 > {
+  // eslint-disable-next-line @typescript-eslint/no-useless-constructor
   constructor(
-    @inject(jsonSchemaAdapter.config.types.validator.AJV) ajv: Ajv.Ajv,
+    @inject(
+      GAME_ADAPTER_TYPES.api.validator.schema.query.card
+        .CARD_FIND_QUERY_API_V1_JOI_VALIDATOR_SCHEMA,
+    )
+    cardFindQueryApiV1JoyValidatorSchema: Joi.ObjectSchema<CardFindQueryApiV1>,
   ) {
-    super(
-      ajv,
-      join(
-        __dirname,
-        '..',
-        '..',
-        '..',
-        'json-schema',
-        'CardFindQueryApiV1.schema',
-      ),
-    );
+    super(cardFindQueryApiV1JoyValidatorSchema);
   }
 }
