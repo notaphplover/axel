45e76ad09d0fcc349b2d7961b2121d55abbb54d0
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2020-10-25 19:06:23 +0100

    feat(db): update db module to expose MongoosePaginatedSearchRepository and MongooseProjectionPaginatedSearchRepository

--- src/layer-modules/db/adapter/index.ts
+++ src/layer-modules/db/adapter/index.ts
@@ -1,10 +1,18 @@
 import { DB_ADAPTER_PUBLIC_TYPES } from './config/types';
 import { MongooseConector } from './MongooseConnector';
 import { MongooseInsertRepository } from './MongooseInsertRepository';
+import { MongoosePaginatedSearchRepository } from './MongoosePaginatedSearchRepository';
+import { MongooseProjectionPaginatedSearchRepository } from './MongooseProjectionPaginatedSearchRepository';
 import { MongooseSearchRepository } from './MongooseSearchRepository';
 import { dbContainer } from './config/container';
 
-export { MongooseConector, MongooseInsertRepository, MongooseSearchRepository };
+export {
+  MongooseConector,
+  MongooseInsertRepository,
+  MongoosePaginatedSearchRepository,
+  MongooseProjectionPaginatedSearchRepository,
+  MongooseSearchRepository,
+};
 
 // eslint-disable-next-line @typescript-eslint/typedef
 export const dbAdapter = {
