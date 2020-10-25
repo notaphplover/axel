fffb8b6017e7cfb4fee65ac3c52b9794682bd7d1
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2020-10-25 18:27:58 +0100

    test(game): add game setup fixtures

--- src/data-modules/game/test/fixtures/adapter/api/model/setup.ts
+++ src/data-modules/game/test/fixtures/adapter/api/model/setup.ts
@@ -3,14 +3,20 @@ import {
   FixtureFactory,
 } from '../../../../../../../common/test';
 import { extendedGameSetup, playerSetup } from '../../../domain/model/setup';
+import { BasicGameSetupApiV1 } from '../../../../../adapter/api/model/setup/BasicGameSetupApiV1';
 import { ExtendedGameSetupApiV1 } from '../../../../../adapter/api/model/setup/ExtendedGameSetupApiV1';
 import { GameFormatApiV1 } from '../../../../../adapter/api/model/GameFormatApiV1';
+import { PlayerReferenceApiV1 } from '../../../../../adapter/api/model/setup/PlayerReferenceApiV1';
 import { PlayerSetupApiV1 } from '../../../../../adapter/api/model/setup/PlayerSetupApiV1';
 import { cardDeckApiV1 } from './deck';
 
 export const playerSetupApiV1: PlayerSetupApiV1 = {
   deck: cardDeckApiV1,
   userId: playerSetup.userId,
+};
+
+export const playerReferenceApiV1: PlayerReferenceApiV1 = {
+  userId: playerSetupApiV1.userId,
 };
 
 export const extendedGameSetupApiV1: ExtendedGameSetupApiV1 = {
@@ -23,4 +29,16 @@ export const extendedGameSetupApiV1: ExtendedGameSetupApiV1 = {
 
 export const extendedGameSetupApiV1FixtureFactory: FixtureFactory<ExtendedGameSetupApiV1> = new DeepCloneFixtureFactory(
   extendedGameSetupApiV1,
+);
+
+export const basicGameSetupApiV1: BasicGameSetupApiV1 = {
+  format: GameFormatApiV1.UNRESTRICTED,
+  id: extendedGameSetup.id,
+  ownerUserId: extendedGameSetup.ownerUserId,
+  playerSetups: [playerSetupApiV1],
+  playerSlots: extendedGameSetup.playerSlots,
+};
+
+export const basicGameSetupApiV1FixtureFactory: FixtureFactory<BasicGameSetupApiV1> = new DeepCloneFixtureFactory(
+  basicGameSetupApiV1,
 );
--- src/data-modules/game/test/fixtures/adapter/api/query/setup.ts
+++ src/data-modules/game/test/fixtures/adapter/api/query/setup.ts
@@ -4,16 +4,18 @@ import {
 } from '../../../../../../../common/test';
 import { GameFormatApiV1 } from '../../../../../adapter/api/model/GameFormatApiV1';
 import { GameSetupCreationQueryApiV1 } from '../../../../../adapter/api/query/setup/GameSetupCreationQueryApiV1';
+import { GameSetupFindQueryApiV1 } from '../../../../../adapter/api/query/setup/GameSetupFindQueryApiV1';
+import { GameSetupFindQueryPlayerSetupApiV1 } from '../../../../../adapter/api/query/setup/GameSetupFindQueryPlayerSetupApiV1';
 import { cardDeck } from '../../../domain/model/deck';
-import { user } from '../../../../../../user/test/fixtures/domain/model/fixtures';
+import { extendedGameSetup } from '../../../domain/model/setup';
 
 export const gameSetupCreationQueryApiV1: GameSetupCreationQueryApiV1 = {
   format: GameFormatApiV1.UNRESTRICTED,
-  ownerUserId: user.id,
+  ownerUserId: extendedGameSetup.ownerUserId,
   playerSetups: [
     {
       deckId: cardDeck.id,
-      userId: user.id,
+      userId: extendedGameSetup.ownerUserId,
     },
   ],
   playerSlots: 2,
@@ -21,4 +23,20 @@ export const gameSetupCreationQueryApiV1: GameSetupCreationQueryApiV1 = {
 
 export const gameSetupCreationQueryApiV1FixtureFactory: FixtureFactory<GameSetupCreationQueryApiV1> = new DeepCloneFixtureFactory(
   gameSetupCreationQueryApiV1,
+);
+
+export const gameSetupFindQueryPlayerSetupApiV1: GameSetupFindQueryPlayerSetupApiV1 = {
+  userId: extendedGameSetup.ownerUserId,
+};
+
+export const gameSetupFindQueryApiV1: GameSetupFindQueryApiV1 = {
+  format: GameFormatApiV1.UNRESTRICTED,
+  id: extendedGameSetup.id,
+  ownerUserId: extendedGameSetup.ownerUserId,
+  playerSetups: [gameSetupFindQueryPlayerSetupApiV1],
+  playerSlots: extendedGameSetup.playerSlots,
+};
+
+export const gameSetupFindQueryApiV1FixtureFactory: FixtureFactory<GameSetupFindQueryApiV1> = new DeepCloneFixtureFactory(
+  gameSetupFindQueryApiV1,
 );
