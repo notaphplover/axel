b359aad3ec54a5499d52c2abb92b4ba1dd70d119
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2020-10-26 08:12:14 +0100

    chore(jest): refactor jest config to reduce duplicity

--- jest.config.js
+++ jest.config.js
@@ -1,93 +1,54 @@
 /** https://jestjs.io/docs/en/configuration */
+const collectCoverageFrom = ['**/*.ts'];
+const coveragePathIgnorePatterns = [
+  '/node_modules/',
+  '<rootDir>/*/test/',
+  '<rootDir>/data-modules/*/test/',
+  '<rootDir>/layer-modules/*/test/',
+];
+const coverageThreshold = {
+  global: {
+    branches: 70,
+    functions: 70,
+    lines: 70,
+    statements: 70,
+  },
+};
+const moduleFileExtensions = ['ts', 'tsx', 'js', 'jsx', 'json', 'node'];
+const rootDir = 'src';
+const testEnvironment = 'node';
+
+const testMatchGenerator = (dirName) =>  [
+  `<rootDir>/*/test/${dirName}/**/*.spec.ts`,
+  `<rootDir>/data-modules/*/test/${dirName}/**/*.spec.ts`,
+  `<rootDir>/layer-modules/*/test/${dirName}/**/*.spec.ts`,
+];
+
+const testPathIgnorePatterns = ['/node_modules'];
+
+const transform = {
+  '^.+\\.ts?$': 'ts-jest',
+};
+
+const jestProjectConfigGenerator = (displayName, testDirName) => {
+  return {
+    displayName: displayName,
+    collectCoverageFrom: collectCoverageFrom,
+    coveragePathIgnorePatterns: coveragePathIgnorePatterns,
+    coverageThreshold: coverageThreshold,
+    moduleFileExtensions: moduleFileExtensions,
+    rootDir: rootDir,
+    testEnvironment: testEnvironment,
+    testMatch: testMatchGenerator(testDirName),
+    testPathIgnorePatterns: testPathIgnorePatterns,
+    transform: transform,
+  };
+};
+
 module.exports = {
   projects: [
-    {
-      displayName: 'Unit',
-      collectCoverageFrom: ['**/*.ts'],
-      coveragePathIgnorePatterns: [
-        '/node_modules/',
-        '<rootDir>/*/test/',
-        '<rootDir>/data-modules/*/test/',
-        '<rootDir>/layer-modules/*/test/',
-      ],
-      coverageThreshold: {
-        global: {
-          branches: 70,
-          functions: 70,
-          lines: 70,
-          statements: 70,
-        },
-      },
-      moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-      rootDir: 'src',
-      testEnvironment: 'node',
-      testMatch: [
-        '<rootDir>/*/test/unit/**/*.spec.ts',
-        '<rootDir>/data-modules/*/test/unit/**/*.spec.ts',
-        '<rootDir>/layer-modules/*/test/unit/**/*.spec.ts',
-      ],
-      testPathIgnorePatterns: ['/node_modules'],
-      transform: {
-        '^.+\\.ts?$': 'ts-jest',
-      },
-    },
-    {
-      displayName: 'Integration',
-      collectCoverageFrom: ['**/*.ts'],
-      coveragePathIgnorePatterns: [
-        '/node_modules/',
-        '<rootDir>/*/test/',
-        '<rootDir>/data-modules/*/test/',
-        '<rootDir>/layer-modules/*/test/',
-      ],
-      coverageThreshold: {
-        global: {
-          branches: 70,
-          functions: 70,
-          lines: 70,
-          statements: 70,
-        },
-      },
-      moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-      rootDir: 'src',
-      testEnvironment: 'node',
-      testMatch: [
-        '<rootDir>/*/test/integration/**/*.spec.ts',
-        '<rootDir>/data-modules/*/test/integration/**/*.spec.ts',
-        '<rootDir>/layer-modules/*/test/integration/**/*.spec.ts',
-      ],
-      transform: {
-        '^.+\\.ts?$': 'ts-jest',
-      },
-    },
-    {
-      displayName: 'End2End',
-      collectCoverageFrom: ['**/*.ts'],
-      coveragePathIgnorePatterns: [
-        '/node_modules/',
-        '<rootDir>/*/test/',
-        '<rootDir>/data-modules/*/test/',
-        '<rootDir>/layer-modules/*/test/',
-      ],
-      coverageThreshold: {
-        global: {
-          branches: 70,
-          functions: 70,
-          lines: 70,
-          statements: 70,
-        },
-      },
-      moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-      rootDir: 'src',
-      testEnvironment: 'node',
-      testMatch: [
-        '<rootDir>/*/test/end-to-end/**/*.spec.ts',
-        '<rootDir>/data-modules/*/test/end-to-end/**/*.spec.ts',
-        '<rootDir>/layer-modules/*/test/end-to-end/**/*.spec.ts',
-      ],
-      transform: {
-        '^.+\\.ts?$': 'ts-jest',
-      },
-    },
+    jestProjectConfigGenerator('Unit', 'unit'),
+    jestProjectConfigGenerator('Integration', 'integration'),
+    jestProjectConfigGenerator('End2End', 'end-to-end'),
   ],
 };
