f2b39f005b327264c2b671e8d675bbaaf9a28176
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2020-10-29 01:04:38 +0100

    feat(task-graph): update BaseTaskGraphNode to handle inner status

--- src/data-modules/task-graph/domain/BaseTaskGraphNode.ts
+++ src/data-modules/task-graph/domain/BaseTaskGraphNode.ts
@@ -3,25 +3,52 @@ import { Capsule } from '../../../common/domain';
 
 export abstract class BaseTaskGraphNode<TId, TOutput>
   implements TaskGraphNode<TId, TOutput> {
-  protected innerOutput: Capsule<TOutput> | null;
-
-  protected innerStatus: TaskGraphNodeStatus;
+  private innerOutput: Capsule<TOutput> | null;
+  private innerPerformPromise: Promise<null | Capsule<TOutput>> | undefined;
+  private innerStatus: TaskGraphNodeStatus;
 
   constructor(
     public readonly dependsOn: Iterable<TId>,
     public readonly id: TId,
   ) {
-    this.innerStatus = TaskGraphNodeStatus.NotStarted;
     this.innerOutput = null;
+    this.innerPerformPromise = undefined;
+    this.innerStatus = TaskGraphNodeStatus.NotStarted;
   }
 
   public getOutput(): Capsule<TOutput> | null {
     return this.innerOutput !== null ? { elem: this.innerOutput.elem } : null;
   }
 
-  public abstract perform(): Promise<void>;
+  public async perform(): Promise<void> {
+    if (this.innerStatus !== TaskGraphNodeStatus.NotStarted) {
+      await this.innerPerformPromise;
+      return;
+    }
+
+    this.innerStatus = TaskGraphNodeStatus.InProgress;
+
+    this.innerPerformPromise = this.getPerformPromise();
+
+    this.innerOutput = await this.innerPerformPromise;
+  }
 
   public get status(): TaskGraphNodeStatus {
     return this.innerStatus;
   }
+
+  private async getPerformPromise(): Promise<null | Capsule<TOutput>> {
+    try {
+      const innerPerformOutput: TOutput = await this.innerPerform();
+
+      this.innerStatus = TaskGraphNodeStatus.Ended;
+
+      return { elem: innerPerformOutput };
+    } catch (err) {
+      this.innerStatus = TaskGraphNodeStatus.Error;
+      return null;
+    }
+  }
+
+  protected abstract innerPerform(): Promise<TOutput>;
 }
