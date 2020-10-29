dfbe05a55c3c85903dd856a51fd90719e8844419
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2020-10-29 01:01:14 +0100

    style(task-graph): add TaskGraphNode doc

--- src/data-modules/task-graph/domain/TaskGraphNode.ts
+++ src/data-modules/task-graph/domain/TaskGraphNode.ts
@@ -1,17 +1,57 @@
 import { Capsule } from '../../../common/domain';
 
+/**
+ * Represents the status of a task node
+ */
 export enum TaskGraphNodeStatus {
+  /**
+   * The task is successfully accomplished with no errors.
+   */
   Ended,
+  /**
+   * The task could not be accomplished due to some errors.
+   */
   Error,
+  /**
+   * The task is being accomplished.
+   */
   InProgress,
+  /**
+   * The task is not started.
+   */
   NotStarted,
 }
 
+/**
+ * Repreesnts a task node of a task graph
+ */
 export interface TaskGraphNode<TId, TOutput> {
+  /**
+   * Id of the tasks which must be completed before this task can be accomplished.
+   */
   readonly dependsOn: Iterable<TId>;
+  /**
+   * Task id
+   */
   readonly id: TId;
+  /**
+   * Task status
+   */
   readonly status: TaskGraphNodeStatus;
-
+  /**
+   * Gets the task result.
+   *
+   * - If the status of this task is not Ended, this method must return null.
+   * - If the status of this task is Ended, this method must return the output of the task inside a Capsule
+   */
   getOutput(): Capsule<TOutput> | null;
+  /**
+   * Performs the task.
+   *
+   * If the task is not started, starts the task.
+   * If the task is started, returns a Promise of the task being accomplished without starting it again.
+   *
+   * This method should never throw an error.
+   */
   perform(): Promise<void>;
 }
