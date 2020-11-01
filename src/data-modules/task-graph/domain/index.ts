501b328e2b84b9519db612d31ef7fa0b948f06db
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2020-11-01 11:04:14 +0100

    feat(task-graph): update task-graph module to expose QueueBasedTaskGraph and TaskGraph

--- src/data-modules/task-graph/domain/index.ts
+++ src/data-modules/task-graph/domain/index.ts
@@ -1,4 +1,12 @@
 import { TaskGraphNode, TaskGraphNodeStatus } from './TaskGraphNode';
 import { BaseTaskGraphNode } from './BaseTaskGraphNode';
+import { QueueBasedTaskGraph } from './QueueBasedTaskGraph';
+import { TaskGraph } from './TaskGraph';
 
-export { BaseTaskGraphNode, TaskGraphNode, TaskGraphNodeStatus };
+export {
+  BaseTaskGraphNode,
+  QueueBasedTaskGraph,
+  TaskGraph,
+  TaskGraphNode,
+  TaskGraphNodeStatus,
+};
