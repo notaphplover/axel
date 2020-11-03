30cf600edc812debea09923ddd9132f95ebced5a
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2020-11-03 23:42:53 +0100

    test(user): add userTestE2eContainer

---
+++ src/data-modules/user/test/config/container/e2eContainer.ts
@@ -0,0 +1,15 @@
+import { ContainerModule, interfaces } from 'inversify';
+import { CreateUserDbTaskGraphNode } from '../../task-graph/CreateUserDbTaskGraphNode';
+import { USER_ADAPTER_E2E_TYPES } from '../types/e2eTypes';
+
+export function bindE2E(bind: interfaces.Bind): void {
+  bind(USER_ADAPTER_E2E_TYPES.CREATE_USER_DB_TASK_GRAPH_NODE).to(
+    CreateUserDbTaskGraphNode,
+  );
+}
+
+export const userTestE2eContainer: ContainerModule = new ContainerModule(
+  (bind: interfaces.Bind) => {
+    bindE2E(bind);
+  },
+);
---
+++ src/data-modules/user/test/config/types/e2eTypes.ts
@@ -0,0 +1,4 @@
+// eslint-disable-next-line @typescript-eslint/typedef
+export const USER_ADAPTER_E2E_TYPES = {
+  CREATE_USER_DB_TASK_GRAPH_NODE: Symbol.for('CreateUserDbTaskGraphNode'),
+};
