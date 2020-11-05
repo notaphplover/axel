c1cc1a5a59673989bca766b25e12cf7b3d588b96
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2020-11-05 14:27:56 +0100

    test(common): add CURRENT_TASK_GRAPH symbol

--- src/common/test/config/types/e2ETypes.ts
+++ src/common/test/config/types/e2ETypes.ts
@@ -1,5 +1,8 @@
 // eslint-disable-next-line @typescript-eslint/typedef
 export const COMMON_E2E_TYPES = {
+  taskGraph: {
+    CURRENT_TASK_GRAPH: Symbol.for('CurrentTaskgGraph'),
+  },
   utils: {
     NUMERIC_SEQUENCE: Symbol.for('NumericSequence'),
   },
