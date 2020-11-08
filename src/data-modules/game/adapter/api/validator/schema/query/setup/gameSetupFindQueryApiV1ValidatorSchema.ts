4063c732fe8055b9abc35874c69401f4469ead93
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2020-11-08 22:58:27 +0100

    fix(game): fix gameSetupFindQueryApiV1JoyValidatorSchema to validate limit and offset

--- src/data-modules/game/adapter/api/validator/schema/query/setup/gameSetupFindQueryApiV1ValidatorSchema.ts
+++ src/data-modules/game/adapter/api/validator/schema/query/setup/gameSetupFindQueryApiV1ValidatorSchema.ts
@@ -3,11 +3,20 @@ import Joi from 'joi';
 import { gameFormatApiV1JoyValidatorSchema } from '../../model/gameFormatApiV1JoiValidatorSchema';
 import { gameSetupFindQueryPlayerSetupApiV1JoyValidatorSchema } from './gameSetupFindQueryPlayerSetupApiV1ValidatorSchema';
 
+const GAME_SETUP_FIND_QUERY_API_V1_LIMIT_MAX_VALUE: number = 100;
+
 export const gameSetupFindQueryApiV1JoyValidatorSchema: Joi.ObjectSchema<GameSetupFindQueryApiV1> = Joi.object<
   GameSetupFindQueryApiV1
 >({
   format: gameFormatApiV1JoyValidatorSchema.optional(),
   id: Joi.string().optional(),
+  limit: Joi.number()
+    .strict()
+    .integer()
+    .positive()
+    .max(GAME_SETUP_FIND_QUERY_API_V1_LIMIT_MAX_VALUE)
+    .required(),
+  offset: Joi.number().strict().integer().min(0).required(),
   ownerUserId: Joi.string().optional(),
   playerSetups: Joi.array()
     .items(gameSetupFindQueryPlayerSetupApiV1JoyValidatorSchema)
