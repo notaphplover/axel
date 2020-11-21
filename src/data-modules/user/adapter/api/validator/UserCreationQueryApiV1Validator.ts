76d450753ed7f362a0564bea9eae124ca8431522
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2020-11-22 00:22:45 +0100

    refactor(user): update UserCreationQueryApiV1Validator to extend JoiObjectValidator

--- src/data-modules/user/adapter/api/validator/UserCreationQueryApiV1Validator.ts
+++ src/data-modules/user/adapter/api/validator/UserCreationQueryApiV1Validator.ts
@@ -1,26 +1,19 @@
 import { inject, injectable } from 'inversify';
-import Ajv from 'ajv';
-import { JsonSchemaFileValidator } from '../../../../../json-schema/adapter';
+import Joi from 'joi';
+import { JoiObjectValidator } from '../../../../../integration-modules/joi/adapter';
+import { USER_ADAPTER_TYPES } from '../../config/types';
 import { UserCreationQueryApiV1 } from '../query/UserCreationQueryApiV1';
-import { join } from 'path';
-import { jsonSchemaAdapter } from '../../../../../json-schema/adapter';
 
 @injectable()
-export class UserCreationQueryApiV1Validator extends JsonSchemaFileValidator<
-  UserCreationQueryApiV1
-> {
+export class UserCreationQueryApiV1Validator extends JoiObjectValidator<UserCreationQueryApiV1> {
+  // eslint-disable-next-line @typescript-eslint/no-useless-constructor
   constructor(
-    @inject(jsonSchemaAdapter.config.types.validator.AJV) ajv: Ajv.Ajv,
+    @inject(
+      USER_ADAPTER_TYPES.api.validator.schema
+        .USER_CREATION_QUERY_API_V1_JOI_VALIDATOR,
+    )
+    userCreationQueryApiV1JoiValidator: Joi.ObjectSchema<UserCreationQueryApiV1>,
   ) {
-    super(
-      ajv,
-      join(
-        __dirname,
-        '..',
-        '..',
-        'json-schema',
-        'UserCreationQueryApiV1.schema',
-      ),
-    );
+    super(userCreationQueryApiV1JoiValidator);
   }
 }
