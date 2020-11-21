e11fcc60f8bc481f4900af13f6cea82d628429b0
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2020-11-22 00:00:26 +0100

    feat(game): add resourceApiV1JoiValidatorSchema

---
+++ src/data-modules/game/adapter/api/validator/schema/model/card/resourceApiV1JoiValidatorSchema.ts
@@ -0,0 +1,14 @@
+import Joi from 'joi';
+import { ResourceApiV1 } from '../../../../model/card/ResourceApiV1';
+import { ResourceTypeApiV1 } from '../../../../model/card/ResourceTypeApiV1';
+
+export const resourceApiV1JoiValidatorSchema: Joi.ObjectSchema<ResourceApiV1> = Joi.object<ResourceApiV1>(
+  {
+    [ResourceTypeApiV1.Black]: Joi.number().strict().required(),
+    [ResourceTypeApiV1.Blue]: Joi.number().strict().required(),
+    [ResourceTypeApiV1.Green]: Joi.number().strict().required(),
+    [ResourceTypeApiV1.Red]: Joi.number().strict().required(),
+    [ResourceTypeApiV1.Uncolored]: Joi.number().strict().required(),
+    [ResourceTypeApiV1.White]: Joi.number().strict().required(),
+  },
+);
