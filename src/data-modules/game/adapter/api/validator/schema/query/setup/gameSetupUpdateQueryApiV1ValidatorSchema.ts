d90481f47361c8ff362021e311d0ecdd3815b82f
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2020-11-25 17:53:49 +0100

    feat(game): update gameSetupUpdateQueryApiV1JoiValidatorSchema with removePlayerSetups property

--- src/data-modules/game/adapter/api/validator/schema/query/setup/gameSetupUpdateQueryApiV1ValidatorSchema.ts
+++ src/data-modules/game/adapter/api/validator/schema/query/setup/gameSetupUpdateQueryApiV1ValidatorSchema.ts
@@ -1,15 +1,21 @@
 import { GameSetupUpdateQueryApiV1 } from '../../../../query/setup/GameSetupUpdateQueryApiV1';
 import Joi from 'joi';
 import { gameSetupUpdateQueryAdditionalPlayerSetupApiV1ValidatorSchema } from './gameSetupUpdateQueryAdditionalPlayerSetupApiV1ValidatorSchema';
+import { playerReferenceApiV1JoiValidatorSchema } from '../../model/schema/playerReferenceApiV1JoiValidatorSchema';
 
-const GAME_SETUP_CREATION_QUERY_PLAYER_SETUPS_LENGTH: number = 1;
+const ADDITIONAL_PLAYER_SETUPS_LENGTH: number = 1;
+const REMOVE_PLAYER_SETUPS_LENGTH: number = 1;
 
 export const gameSetupUpdateQueryApiV1JoiValidatorSchema: Joi.ObjectSchema<GameSetupUpdateQueryApiV1> = Joi.object<GameSetupUpdateQueryApiV1>(
   {
     additionalPlayerSetups: Joi.array()
       .items(gameSetupUpdateQueryAdditionalPlayerSetupApiV1ValidatorSchema)
-      .length(GAME_SETUP_CREATION_QUERY_PLAYER_SETUPS_LENGTH)
+      .length(ADDITIONAL_PLAYER_SETUPS_LENGTH)
       .optional(),
     id: Joi.string().required(),
+    removePlayerSetups: Joi.array()
+      .items(playerReferenceApiV1JoiValidatorSchema)
+      .length(REMOVE_PLAYER_SETUPS_LENGTH)
+      .optional(),
   },
 );
