eaaae39898028e71af312b49fa7ee8854fe82c87
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2020-11-29 00:04:45 +0100

    feat(mongoose): update mongoose adapter bindings to inject connector in a single scope

--- src/integration-modules/mongoose/adapter/config/container.ts
+++ src/integration-modules/mongoose/adapter/config/container.ts
@@ -3,7 +3,9 @@ import { MONGOOSE_ADAPTER_TYPES } from './types';
 import { MongooseConnector } from '../MongooseConnector';
 
 function bindAdapter(bind: interfaces.Bind): void {
-  bind(MONGOOSE_ADAPTER_TYPES.db.MONGOOSE_CONNECTOR).to(MongooseConnector);
+  bind(MONGOOSE_ADAPTER_TYPES.db.MONGOOSE_CONNECTOR)
+    .to(MongooseConnector)
+    .inSingletonScope();
 }
 
 export const mongooseContainer: ContainerModule = new ContainerModule(
