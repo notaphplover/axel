1bec2f1e7debe28affad80161a979cb43d41438b
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2020-12-11 15:24:39 +0100

    feat(mongodb): add MongoDbInsertRepository

---
+++ src/integration-modules/mongodb/adapter/MongoDbInsertRepository.ts
@@ -0,0 +1,77 @@
+import { injectable, unmanaged } from 'inversify';
+import { Converter } from '../../../common/domain';
+import { Document } from '../domain/model/Document';
+import { EntitiesNotCreatedError } from '../../../layer-modules/db/domain';
+import { MongoDbConnector } from './MongoDbConnector';
+import mongodb from 'mongodb';
+
+const MONGODB_DUPLICATED_KEY_ERR_CODE: number = 11000;
+
+@injectable()
+export abstract class MongoDbInsertRepository<
+  TModel,
+  TModelDb extends Document,
+  TQuery
+> {
+  private innerCollection: mongodb.Collection<TModelDb> | undefined;
+
+  constructor(
+    @unmanaged()
+    protected readonly collectionName: string,
+    @unmanaged()
+    protected readonly modelDbToModelConverter: Converter<
+      TModelDb,
+      TModel | Promise<TModel>
+    >,
+    @unmanaged()
+    protected readonly mongoDbConnector: MongoDbConnector,
+    protected readonly queryToInputModelDbs: Converter<
+      TQuery,
+      mongodb.OptionalId<TModelDb>[] | Promise<mongodb.OptionalId<TModelDb>[]>
+    >,
+  ) {}
+
+  private get collection(): mongodb.Collection<TModelDb> {
+    if (this.innerCollection === undefined) {
+      this.innerCollection = this.mongoDbConnector.db.collection<TModelDb>(
+        this.collectionName,
+      );
+    }
+
+    return this.innerCollection;
+  }
+
+  public async insert(query: TQuery): Promise<TModel[]> {
+    const entitiesDbToInsert: mongodb.OptionalId<TModelDb>[] = await this.queryToInputModelDbs.transform(
+      query,
+    );
+
+    let entitiesDbCreated: TModelDb[];
+    try {
+      const entitiesDbInsertionOperation: mongodb.InsertWriteOpResult<
+        mongodb.WithId<TModelDb>
+      > = await this.collection.insertMany(entitiesDbToInsert);
+
+      entitiesDbCreated = entitiesDbInsertionOperation.ops as TModelDb[];
+    } catch (err) {
+      if (
+        err instanceof mongodb.MongoError &&
+        err.code === MONGODB_DUPLICATED_KEY_ERR_CODE
+      ) {
+        throw new EntitiesNotCreatedError(
+          'Entities could not be created due to a duplicated key error',
+        );
+      } else {
+        throw err;
+      }
+    }
+
+    const entitiesCreated: TModel[] = await Promise.all(
+      entitiesDbCreated.map((entityDb: TModelDb) =>
+        this.modelDbToModelConverter.transform(entityDb),
+      ),
+    );
+
+    return entitiesCreated;
+  }
+}
---
+++ src/integration-modules/mongodb/test/integration/adapter/MongoDbInsertRepository.spec.ts
@@ -0,0 +1,112 @@
+/* eslint-disable @typescript-eslint/unbound-method */
+import 'reflect-metadata';
+import { Capsule, Converter } from '../../../../../common/domain';
+import { Document } from '../../../domain/model/Document';
+import { MongoDbConnector } from '../../../adapter/MongoDbConnector';
+import { MongoDbInsertRepository } from '../../../adapter/MongoDbInsertRepository';
+import { dbTest } from '../../../../../layer-modules/db/test';
+import mongodb from 'mongodb';
+
+class ModelMock {
+  constructor(public foo: string) {}
+}
+
+class InsertQueryMock {
+  constructor(public foo: string) {}
+}
+
+type ModelMockDb = ModelMock & Document;
+
+class MongoDbInsertRepositoryMock extends MongoDbInsertRepository<
+  ModelMock,
+  ModelMockDb,
+  InsertQueryMock
+> {}
+
+const collectionName: string = 'MongoDbInsertRepositoryIntegrationTests';
+
+const mongodbIntegrationDescribeGenerator: (
+  output: Capsule<MongoDbConnector | undefined>,
+) => jest.Describe =
+  dbTest.integration.utils.mongoDbIntegrationDescribeGenerator;
+
+const outputParam: Capsule<MongoDbConnector | undefined> = { elem: undefined };
+
+mongodbIntegrationDescribeGenerator(outputParam)(
+  MongoDbInsertRepository.name,
+  () => {
+    let modelDbToModelConverter: Converter<
+      ModelMockDb,
+      ModelMock | Promise<ModelMock>
+    >;
+    let mongoDbConnector: MongoDbConnector;
+
+    let queryToInputModelDbs: Converter<
+      InsertQueryMock,
+      | mongodb.OptionalId<ModelMockDb>[]
+      | Promise<mongodb.OptionalId<ModelMockDb>[]>
+    >;
+
+    let mongoDbInsertRepository: MongoDbInsertRepositoryMock;
+
+    beforeAll(() => {
+      modelDbToModelConverter = {
+        transform: jest.fn(),
+      };
+
+      mongoDbConnector = outputParam.elem as MongoDbConnector;
+
+      queryToInputModelDbs = {
+        transform: jest.fn(),
+      };
+
+      mongoDbInsertRepository = new MongoDbInsertRepositoryMock(
+        collectionName,
+        modelDbToModelConverter,
+        mongoDbConnector,
+        queryToInputModelDbs,
+      );
+    });
+
+    describe('.insert()', () => {
+      let result: unknown;
+
+      let insertQueryMock: InsertQueryMock;
+      let modelMockFixture: ModelMock;
+
+      beforeAll(async () => {
+        modelMockFixture = {
+          foo: 'bar',
+        };
+
+        insertQueryMock = {
+          foo: modelMockFixture.foo,
+        };
+
+        (modelDbToModelConverter.transform as jest.Mock).mockResolvedValueOnce(
+          modelMockFixture,
+        );
+
+        (queryToInputModelDbs.transform as jest.Mock).mockResolvedValueOnce([
+          {
+            foo: modelMockFixture.foo,
+          },
+        ]);
+
+        result = await mongoDbInsertRepository.insert(insertQueryMock);
+      });
+
+      it('must call modelDbToModelConverter.transform with the db entity received', () => {
+        expect(modelDbToModelConverter.transform).toHaveBeenCalledTimes(1);
+        expect(modelDbToModelConverter.transform).toHaveBeenCalledWith({
+          _id: expect.any(mongodb.ObjectID) as mongodb.ObjectID,
+          foo: modelMockFixture.foo,
+        });
+      });
+
+      it('must return domain entities tranformed from db entities', () => {
+        expect(result).toStrictEqual([modelMockFixture]);
+      });
+    });
+  },
+);
