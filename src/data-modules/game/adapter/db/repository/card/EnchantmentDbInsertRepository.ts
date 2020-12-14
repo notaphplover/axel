586cc41615eebeb2af32ef7badf6454a5ed7c85c
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2020-12-14 17:57:39 +0100

    feat(game): update EnchantmentDbInsertRepository to be a MongoDbInsertRepository

--- src/data-modules/game/adapter/db/repository/card/EnchantmentDbInsertRepository.ts
+++ src/data-modules/game/adapter/db/repository/card/EnchantmentDbInsertRepository.ts
@@ -1,39 +1,46 @@
+import {
+  MongoDbConnector,
+  mongodbAdapter,
+} from '../../../../../../integration-modules/mongodb/adapter';
 import { inject, injectable } from 'inversify';
 import { Converter } from '../../../../../../common/domain';
 import { Enchantment } from '../../../../domain/model/card/Enchantment';
 import { EnchantmentCreationQuery } from '../../../../domain/query/card/EnchantmentCreationQuery';
 import { EnchantmentDb } from '../../model/card/EnchantmentDb';
 import { GAME_ADAPTER_TYPES } from '../../../config/types';
-import { Model } from 'mongoose';
-import { MongooseInsertRepository } from '../../../../../../integration-modules/mongoose/adapter';
+import { MongoDbInsertRepository } from '../../../../../../integration-modules/mongodb/adapter/MongoDbInsertRepository';
+import mongodb from 'mongodb';
 
 @injectable()
-export class EnchantmentDbInsertRepository extends MongooseInsertRepository<
+export class EnchantmentDbInsertRepository extends MongoDbInsertRepository<
   Enchantment,
-  EnchantmentCreationQuery,
-  EnchantmentDb
+  EnchantmentDb,
+  EnchantmentCreationQuery
 > {
   // eslint-disable-next-line @typescript-eslint/no-useless-constructor
   constructor(
-    @inject(GAME_ADAPTER_TYPES.db.model.card.ENCHANTMENT_DB_MODEL)
-    model: Model<EnchantmentDb>,
+    @inject(GAME_ADAPTER_TYPES.db.collection.card.CARD_COLLECTION_NAME)
+    collectionName: string,
     @inject(
       GAME_ADAPTER_TYPES.db.converter.card
         .ENCHANTMENT_DB_TO_ENCHANTMENT_CONVERTER,
     )
     enchantmentDbToEnchantmentConverter: Converter<EnchantmentDb, Enchantment>,
+    @inject(mongodbAdapter.config.types.db.MONGODB_CONNECTOR)
+    mongoDbConnector: MongoDbConnector,
     @inject(
       GAME_ADAPTER_TYPES.db.converter.card
         .ENCHANTMENT_CREATION_QUERY_TO_ENCHANTMENT_DBS_CONVERTER,
     )
     enchantmentCreationQueryToEnchantmentDbsConverter: Converter<
       EnchantmentCreationQuery,
-      EnchantmentDb[]
+      mongodb.OptionalId<EnchantmentDb>[]
     >,
   ) {
     super(
-      model,
+      collectionName,
       enchantmentDbToEnchantmentConverter,
+      mongoDbConnector,
       enchantmentCreationQueryToEnchantmentDbsConverter,
     );
   }
