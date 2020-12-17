6dc5ef6aaef6627b5fb9d3b19b50484622813d6c
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2020-12-17 15:08:07 +0100

    feat(user): update UserCreationQueryToUserDbsConverter to be a converter to mongodb.OptionalId<UserDb>[]

--- src/data-modules/user/adapter/db/converter/UserCreationQueryToUserDbsConverter.ts
+++ src/data-modules/user/adapter/db/converter/UserCreationQueryToUserDbsConverter.ts
@@ -1,29 +1,30 @@
 import { inject, injectable } from 'inversify';
 import { Converter } from '../../../../../common/domain';
-import { Model } from 'mongoose';
 import { PasswordHasher } from '../../security/PasswordHasher';
 import { USER_ADAPTER_TYPES } from '../../config/types';
 import { UserCreationQuery } from '../../../domain/query/UserCreationQuery';
 import { UserDb } from '../model/UserDb';
+import mongodb from 'mongodb';
 
 @injectable()
 export class UserCreationQueryToUserDbsConverter
-  implements Converter<UserCreationQuery, Promise<UserDb[]>> {
+  implements
+    Converter<UserCreationQuery, Promise<mongodb.OptionalId<UserDb>[]>> {
   constructor(
-    @inject(USER_ADAPTER_TYPES.db.model.USER_DB_MODEL)
-    private readonly userDbModel: Model<UserDb>,
     @inject(USER_ADAPTER_TYPES.security.PASSWORD_HASHER)
     private readonly passwordHasher: PasswordHasher,
   ) {}
 
-  public async transform(input: UserCreationQuery): Promise<UserDb[]> {
-    return [
-      new this.userDbModel({
-        email: input.email,
-        roles: input.roles,
-        username: input.username,
-        hash: await this.passwordHasher.hashPassword(input.password),
-      }),
-    ];
+  public async transform(
+    input: UserCreationQuery,
+  ): Promise<mongodb.OptionalId<UserDb>[]> {
+    const userDb: mongodb.OptionalId<UserDb> = {
+      email: input.email,
+      hash: await this.passwordHasher.hashPassword(input.password),
+      roles: input.roles,
+      username: input.username,
+    } as mongodb.OptionalId<UserDb>;
+
+    return [userDb];
   }
 }
