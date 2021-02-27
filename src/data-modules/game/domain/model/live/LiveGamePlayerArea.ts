57679e2ab09034293b0827eeae29962bf012e419
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2021-02-27 14:15:20 +0100

    feat(game): update LiveGamePlayerArea with a library property

--- src/data-modules/game/domain/model/live/LiveGamePlayerArea.ts
+++ src/data-modules/game/domain/model/live/LiveGamePlayerArea.ts
@@ -1,9 +1,11 @@
 import { Battlefield } from './Battlefield';
 import { Graveyard } from './Graveyard';
+import { Library } from './Library';
 import { LiveGamePlayer } from './LiveGamePlayer';
 
 export interface LiveGamePlayerArea {
   readonly battlefield: Battlefield;
   readonly graveyard: Graveyard;
+  readonly library: Library;
   readonly player: LiveGamePlayer;
 }
