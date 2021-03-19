547c8f3455d1132e61ab31cb0add4dfdefba6848
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2021-03-19 01:05:36 +0100

    feat(app-ws): add AppWsRequestContext

--- src/data-modules/app-ws/adapter/index.ts
+++ src/data-modules/app-ws/adapter/index.ts
@@ -1,5 +1,11 @@
 import { AppWsMessage } from './model/AppWsMessage';
+import { AppWsRequestContext } from './model/AppWsRequestContext';
 import { AppWsMessageHandler } from './msgHandler/AppWsMessageHandler';
 import { AppWsMessageRouter } from './msgHandler/AppWsMessageRouter';
 
-export { AppWsMessage, AppWsMessageHandler, AppWsMessageRouter };
+export {
+  AppWsMessage,
+  AppWsMessageHandler,
+  AppWsMessageRouter,
+  AppWsRequestContext,
+};
