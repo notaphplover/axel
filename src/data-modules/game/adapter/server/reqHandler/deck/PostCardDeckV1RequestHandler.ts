7e9fd2f04ccfdee21ed9b9b06427fad81c0ced27
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2021-03-20 15:36:20 +0100

    feat(game): update PostCardDeckV1RequestHandler to extend PostEntityRequestHandler

--- src/data-modules/game/adapter/server/reqHandler/deck/PostCardDeckV1RequestHandler.ts
+++ src/data-modules/game/adapter/server/reqHandler/deck/PostCardDeckV1RequestHandler.ts
@@ -7,7 +7,7 @@ import {
   Interactor,
   ValueOrErrors,
 } from '../../../../../../common/domain';
-import { FastifyRequestHandler } from '../../../../../../integration-modules/fastify/adapter';
+import { PostEntityRequestHandler } from '../../../../../../integration-modules/fastify/adapter';
 import { GAME_DOMAIN_TYPES } from '../../../../domain/config/types';
 import { CardDeck } from '../../../../domain/model/deck/CardDeck';
 import { CardDeckCreationQuery } from '../../../../domain/query/deck/CardDeckCreationQuery';
@@ -15,18 +15,20 @@ import { CardDeckApiV1 } from '../../../api/model/deck/CardDeckApiV1';
 import { GAME_ADAPTER_TYPES } from '../../../config/types';
 
 @injectable()
-export class PostCardDeckV1RequestHandler implements FastifyRequestHandler {
+export class PostCardDeckV1RequestHandler extends PostEntityRequestHandler<
+  CardDeck,
+  CardDeckApiV1,
+  CardDeckCreationQuery
+> {
+  // eslint-disable-next-line @typescript-eslint/no-useless-constructor
   constructor(
     @inject(
       GAME_ADAPTER_TYPES.api.converter.deck
         .CARD_DECK_TO_CARD_DECK_API_V1_CONVERTER,
     )
-    private readonly cardDeckToCardDeckApiV1Converter: Converter<
-      CardDeck,
-      CardDeckApiV1
-    >,
+    cardDeckToCardDeckApiV1Converter: Converter<CardDeck, CardDeckApiV1>,
     @inject(GAME_DOMAIN_TYPES.interactor.deck.CREATE_CARD_DECKS_INTERACTOR)
-    private readonly createCardDecksInteractor: Interactor<
+    createCardDecksInteractor: Interactor<
       CardDeckCreationQuery,
       Promise<CardDeck[]>
     >,
@@ -34,38 +36,15 @@ export class PostCardDeckV1RequestHandler implements FastifyRequestHandler {
       GAME_ADAPTER_TYPES.server.converter.deck
         .POST_CARD_DECK_V1_REQUEST_TO_CARD_DECK_CREATION_QUERY_CONVERTER,
     )
-    private readonly postCardDeckV1RequestToCardDeckCreationQueryConverter: Converter<
+    postCardDeckV1RequestToCardDeckCreationQueryConverter: Converter<
       fastify.FastifyRequest,
       Promise<ValueOrErrors<CardDeckCreationQuery>>
     >,
-  ) {}
-
-  public async handle(
-    request: fastify.FastifyRequest,
-    reply: fastify.FastifyReply,
-  ): Promise<void> {
-    const cardDeckCreatioqueryOrErrors: ValueOrErrors<CardDeckCreationQuery> = await this.postCardDeckV1RequestToCardDeckCreationQueryConverter.transform(
-      request,
+  ) {
+    super(
+      cardDeckToCardDeckApiV1Converter,
+      createCardDecksInteractor,
+      postCardDeckV1RequestToCardDeckCreationQueryConverter,
     );
-    if (cardDeckCreatioqueryOrErrors.isEither) {
-      await reply
-        .code(StatusCodes.BAD_REQUEST)
-        .send({ message: cardDeckCreatioqueryOrErrors.value.join('/n') });
-    } else {
-      const cardDeckCreationQuery: CardDeckCreationQuery =
-        cardDeckCreatioqueryOrErrors.value;
-
-      const [
-        cardDeckCreated,
-      ]: CardDeck[] = await this.createCardDecksInteractor.interact(
-        cardDeckCreationQuery,
-      );
-
-      const cardDeckApiV1Created: CardDeckApiV1 = this.cardDeckToCardDeckApiV1Converter.transform(
-        cardDeckCreated,
-      );
-
-      await reply.send(cardDeckApiV1Created);
-    }
   }
 }
