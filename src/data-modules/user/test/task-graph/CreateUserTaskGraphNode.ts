3dbfdfc8b114fccf1f861a9ba6aeefe6936254df
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2021-03-21 18:24:02 +0100

    refactor(user): update CreateUserTaskGraphNode to avoid unsafe index access

--- src/data-modules/user/test/task-graph/CreateUserTaskGraphNode.ts
+++ src/data-modules/user/test/task-graph/CreateUserTaskGraphNode.ts
@@ -1,12 +1,11 @@
 import { injectable, unmanaged } from 'inversify';
 import { v4 as uuidv4 } from 'uuid';
 
-import { Interactor } from '../../../../common/domain';
+import { commonDomain, Interactor } from '../../../../common/domain';
 import { BaseTaskGraphNode } from '../../../task-graph/domain';
 import { User } from '../../domain/model/User';
 import { UserCreationQuery } from '../../domain/query/UserCreationQuery';
 import { userCreationQuery } from '../fixtures/domain/query/fixtures';
-
 
 @injectable()
 export abstract class CreateUserTaskGraphNode<TId> extends BaseTaskGraphNode<
@@ -30,13 +29,19 @@ export abstract class CreateUserTaskGraphNode<TId> extends BaseTaskGraphNode<
   protected async innerPerform(): Promise<User> {
     const uuidV4: string = uuidv4();
 
-    const [userCreated]: User[] = await this.createUsersInteractor.interact({
+    const usersCreated: User[] = await this.createUsersInteractor.interact({
       email: uuidV4 + userCreationQuery.email,
       password: userCreationQuery.password,
       roles: userCreationQuery.roles,
       username: userCreationQuery.username + uuidV4,
     });
 
-    return userCreated;
+    if (commonDomain.utils.hasOneElement(usersCreated)) {
+      const [userCreated]: [User] = usersCreated;
+
+      return userCreated;
+    } else {
+      throw new Error('Expected one entity to be created');
+    }
   }
 }
