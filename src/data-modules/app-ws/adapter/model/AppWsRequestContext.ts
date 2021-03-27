9818bded21b7ec9218b82672dbbc011838d6cd6d
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2021-03-27 17:45:03 +0100

    feat(app-ws): update AppWsRequestContext with playerGateway property

--- src/data-modules/app-ws/adapter/model/AppWsRequestContext.ts
+++ src/data-modules/app-ws/adapter/model/AppWsRequestContext.ts
@@ -1,5 +1,7 @@
+import { Messenger } from '../../../../common/domain';
 import { User } from '../../../user/domain';
 
 export interface AppWsRequestContext {
+  playerGateway: Messenger;
   user: User;
 }
