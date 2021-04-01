015d31a9f9c1fc8686eb8b81bd487df339d2d81a
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2021-04-01 14:25:49 +0200

    feat(app-ws): add WebSocketDataToAppWsRequestcontextConverter

---
+++ src/data-modules/app-ws/adapter/converter/WebSocketDataToAppWsRequestcontextConverter.ts
@@ -0,0 +1,64 @@
+import WebSocket from 'ws';
+
+import { AppWsRequestContext } from '..';
+import { Converter, ValueOrErrors } from '../../../../common/domain';
+import { WsRequestContext } from '../../../../integration-modules/ws/adapter/WsRequestContext';
+import { JwtManager } from '../../../jwt/domain';
+import { User } from '../../../user/domain';
+
+export class WebSocketDataToAppWsRequestcontextConverter
+  implements
+    Converter<
+      WebSocket.Data,
+      Promise<ValueOrErrors<AppWsRequestContext>>,
+      WsRequestContext
+    > {
+  constructor(private readonly jwtManager: JwtManager<User>) {}
+
+  public async transform(
+    webSocketData: WebSocket.Data,
+    context: WsRequestContext,
+  ): Promise<ValueOrErrors<AppWsRequestContext>> {
+    const stringifiedMessageData: string = webSocketData.toString();
+
+    try {
+      const user: User = await this.jwtManager.parse(stringifiedMessageData);
+
+      const appWsRequestContextOrErrors: ValueOrErrors<AppWsRequestContext> = {
+        isEither: false,
+        value: {
+          playerGateway: {
+            send: async (data: unknown) => {
+              return new Promise<void>(
+                (resolve: () => void, reject: (cause: unknown) => void) => {
+                  context.socket.send(data, (err: Error | undefined) => {
+                    if (err === undefined) {
+                      resolve();
+                    } else {
+                      reject(err);
+                    }
+                  });
+                },
+              );
+            },
+          },
+          user: user,
+        },
+      };
+
+      return appWsRequestContextOrErrors;
+    } catch (err: unknown) {
+      const stringifiedErrorMessage: string = JSON.stringify(
+        err,
+        Object.getOwnPropertyNames(err),
+      );
+
+      return {
+        isEither: true,
+        value: [
+          `Unexpected error when parsing auth token. Underlying error:\n\n ${stringifiedErrorMessage}`,
+        ],
+      };
+    }
+  }
+}
--- src/data-modules/app-ws/adapter/index.ts
+++ src/data-modules/app-ws/adapter/index.ts
@@ -1,13 +1,15 @@
 import { QueryWsApiToQueryConverter } from './converter/QueryWsApiToQueryConverter';
+import { WebSocketDataToAppWsRequestcontextConverter } from './converter/WebSocketDataToAppWsRequestcontextConverter';
 import { AppWsRequestContext } from './model/AppWsRequestContext';
 import { QueryWsApi } from './model/QueryWsApi';
 import { AppWsMessageHandler } from './msgHandler/AppWsMessageHandler';
 import { AppWsMessageRouter } from './msgHandler/AppWsMessageRouter';
 
 export {
-  QueryWsApi,
-  QueryWsApiToQueryConverter,
   AppWsMessageHandler,
   AppWsMessageRouter,
   AppWsRequestContext,
+  QueryWsApi,
+  QueryWsApiToQueryConverter,
+  WebSocketDataToAppWsRequestcontextConverter,
 };
