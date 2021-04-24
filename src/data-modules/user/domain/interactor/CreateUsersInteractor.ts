c62e06ac860e50b90cb523cfdbc4ac31d9d21c11
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2021-04-24 16:31:55 +0200

    refactor(user): update CreateUsersInteractor to extend CreateEntitiesInteractor

--- src/data-modules/user/domain/interactor/CreateUsersInteractor.ts
+++ src/data-modules/user/domain/interactor/CreateUsersInteractor.ts
@@ -1,25 +1,21 @@
 import { inject, injectable } from 'inversify';
 
-import { Interactor } from '../../../../common/domain';
+import { CreateEntitiesInteractor } from '../../../../common/domain';
 import { InsertRepository } from '../../../../layer-modules/db/domain';
 import { USER_DOMAIN_TYPES } from '../config/types';
 import { User } from '../model/User';
 import { UserCreationQuery } from '../query/UserCreationQuery';
 
 @injectable()
-export class CreateUsersInteractor
-  implements Interactor<UserCreationQuery, Promise<User[]>> {
+export class CreateUsersInteractor extends CreateEntitiesInteractor<
+  User,
+  UserCreationQuery
+> {
+  // eslint-disable-next-line @typescript-eslint/no-useless-constructor
   constructor(
     @inject(USER_DOMAIN_TYPES.repository.USER_INSERT_REPOSITORY)
-    private readonly userInsertRepository: InsertRepository<
-      User,
-      UserCreationQuery
-    >,
-  ) {}
-
-  public async interact(query: UserCreationQuery): Promise<User[]> {
-    const usersCreated: User[] = await this.userInsertRepository.insert(query);
-
-    return usersCreated;
+    userInsertRepository: InsertRepository<User, UserCreationQuery>,
+  ) {
+    super(userInsertRepository);
   }
 }
