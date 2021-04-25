0649d3d160ffbf5389280dca67bb449ac5175811
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2021-04-25 11:24:49 +0200

    refactor(user): update FindUserInteractor to extend FindEntityInteractor

--- src/data-modules/user/domain/interactor/FindUserInteractor.ts
+++ src/data-modules/user/domain/interactor/FindUserInteractor.ts
@@ -1,23 +1,21 @@
 import { inject, injectable } from 'inversify';
 
-import { Interactor } from '../../../../common/domain';
+import { FindEntityInteractor } from '../../../../common/domain';
 import { SearchRepository } from '../../../../layer-modules/db/domain';
 import { USER_DOMAIN_TYPES } from '../config/types';
 import { User } from '../model/User';
 import { UserFindQuery } from '../query/UserFindQuery';
 
 @injectable()
-export class FindUserInteractor
-  implements Interactor<UserFindQuery, Promise<User | null>> {
+export class FindUserInteractor extends FindEntityInteractor<
+  User,
+  UserFindQuery
+> {
+  // eslint-disable-next-line @typescript-eslint/no-useless-constructor
   constructor(
     @inject(USER_DOMAIN_TYPES.repository.USER_SEARCH_REPOSITORY)
-    private readonly userSearchRepository: SearchRepository<
-      User,
-      UserFindQuery
-    >,
-  ) {}
-
-  public async interact(query: UserFindQuery): Promise<User | null> {
-    return this.userSearchRepository.findOne(query);
+    userSearchRepository: SearchRepository<User, UserFindQuery>,
+  ) {
+    super(userSearchRepository);
   }
 }
