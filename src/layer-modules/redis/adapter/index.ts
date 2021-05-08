62a71ddac9ae9cc798103063a836a364e04000bf
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2021-05-08 22:21:32 +0200

    feat(redis): expose RedisPublisher

--- src/layer-modules/redis/adapter/index.ts
+++ src/layer-modules/redis/adapter/index.ts
@@ -1,8 +1,9 @@
 import { redisContainer } from './config/container';
 import { REDIS_ADAPTER_PUBLIC_TYPES } from './config/types';
+import { RedisPublisher } from './RedisPublisher';
 import { RedisSubscriber } from './RedisSubscriber';
 
-export { RedisSubscriber };
+export { RedisPublisher, RedisSubscriber };
 
 // eslint-disable-next-line @typescript-eslint/typedef
 export const redisAdapter = {
